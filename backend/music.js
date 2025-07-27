d6f1524668777720358087094169a0e57103dabb
Aravindswamymajjuri <aravindswamymajjuri143@gmail.com>
2025-07-27 20:45:50 +0530

    updated music file

--- backend/music.js
+++ backend/music.js
@@ -15,8 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
+    origin: "*"
   }
 });
 
